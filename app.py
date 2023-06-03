a1bd04b2dfb0f1ca84caebb83162c5c1fe83cddc
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-04 00:13:46 +0900

    test

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import dotenv
 import os
 from pymongo import MongoClient
 
+# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
+# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
+SECRET_KEY = 'SPARTA'
+
+# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
+# import jwt
+
+
+# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
+#import datetime
+
+# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
+# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
+import hashlib
+
 dotenv.load_dotenv()
 
 import certifi 
@@ -12,7 +27,7 @@ client = MongoClient(
 )
 
 db = client.dbsparta
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -34,24 +49,26 @@ def login():
         #userid_receive = request.form['userid']
         #userpw_receive = request.form['userpw']
         
-        doc = {"userid": "test", "userpw": "testpassword"}
+        doc = {"userid": "adsasdasda", "userpw": "efewf"}
         db.user.insert_one(doc)
-        for i in db.user.find(): # 배포시 코드 지울 것
-            print(i)
+        
         return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
     
 
 # 회원가입(POST)
 @app.route('/loginregister', methods=['POST'])
 def loginregister():
-    #userid_receive = request.form['userid']
-    #userpw_receive = request.form['userpw']
+    userid_receive = request.form['userid']
+    userpw_receive = request.form['userpw']
     
+    pw_hash = hashlib.sha256(userpw_receive.encode('utf-8')).hexdigest()
+
+
     # 회원가입 db 저장
-    #doc = {"userid": userid_receive, "userpw": userpw_receive}
-    #db.user.insert_one(doc)
+    doc = {"userid": userid_receive, "userpw": pw_hash}
+    db.user.insert_one(doc)
     
-    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+    return redirect(url_for('login'))
 
 # 아이디 중복확인(POST)
 @app.route('/useridchek', methods=['POST'])

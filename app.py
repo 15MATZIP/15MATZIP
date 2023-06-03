2873893d8609de2ebcb56b7ca503ece94f76c60f
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-06-03 21:46:34 +0900

    fst

---
+++ app.py
@@ -0,0 +1,63 @@
+import dotenv
+import os
+from pymongo import MongoClient
+
+dotenv.load_dotenv()
+
+import certifi 
+ca = certifi.where()
+client = MongoClient(
+    os.getenv("MONGO_URL"),
+    tlsCAFile=ca,
+)
+
+db = client.dbsparta
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    #환영페이지 필요없으면
+    #return redirect("/login")
+
+# 로그인페이지(GET)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        #title_receive = request.args.get('title_give') title_give라는 데이터를 가지고옴
+        next = request.args.get('next', '') # login 후 이동할 페이지 지정
+        return render_template('login.html') #jsonify({'result':'success', 'msg': '이 요청은 GET!'}), 
+    else :
+        # 로그인페이지(POST)
+        #userid_receive = request.form['userid']
+        #userpw_receive = request.form['userpw']
+        
+        doc = {"userid": "adsasdasda", "userpw": "efewf"}
+        db.user.insert_one(doc)
+        
+        return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+    
+
+# 회원가입(POST)
+@app.route('/loginregister', methods=['POST'])
+def loginregister():
+    #userid_receive = request.form['userid']
+    #userpw_receive = request.form['userpw']
+    
+    # 회원가입 db 저장
+    #doc = {"userid": userid_receive, "userpw": userpw_receive}
+    #db.user.insert_one(doc)
+    
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+# 아이디 중복확인(POST)
+@app.route('/useridchek', methods=['POST'])
+def useridchek():
+    #title_receive = request.form['title_give']
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+
+if __name__ == '__main__':  
+    app.run('0.0.0.0',port=5001,debug=True)
